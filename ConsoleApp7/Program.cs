be25c05c7b29bb97c3c1a6c3882081e377bf9d72
Danil <d_silchenkov@mail.ru>
2023-09-30 21:39:23 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое однозначное число a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе однозначное число b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введи произведение этих чисел c: ");
+int c = int.Parse(Console.ReadLine());
+if (a * b == c) Console.Write("Молодец, правильно!");
+else
+{
+    Console.Write($"Неправильно, результат произведения чисел {a} и {b} будет равен ");
+    Console.Write(a * b);
+}
